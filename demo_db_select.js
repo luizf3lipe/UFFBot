39f8b0891183f27d866a2827fc5c8e9e8499f9a6
root <root@localhost.localdomain>
2017-12-30 10:13:27 -0200

    trying

--- demo_db_select.js
+++ demo_db_select.js
@@ -7,10 +7,28 @@ var con = mysql.createConnection({
   database: "uffbot"
 });
 
-con.connect(function(err) {
+
+var someVar = [];
+
+con.query("select * from seguranca where id=1", function(err, rows){
+  if(err) {
+    throw err;
+  } else {
+    setValue(rows);
+  }
+});
+
+function setValue(value) {
+  someVar = value;
+  console.log(someVar);
+}
+
+
+
+/*con.connect(function(err) {
   if (err) throw err;
   con.query("SELECT  nome FROM seguranca where id=1", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
   });
-});
+});*/
